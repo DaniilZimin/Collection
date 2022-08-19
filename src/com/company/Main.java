82bbced14779afd771efd04fb0dd32f9654a5a12
dzimin <dan.1996.dan@mail.ru>
2022-08-19 16:11:32 +0500

    Готово!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,26 @@
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        StringList stringList = new StringListImpl();
+        MyList<Integer> myList = new MyListImpl<>();
 
-        System.out.println(stringList.add("Медведь"));
-        System.out.println(stringList.add("Рысь"));
-        System.out.println(stringList.add("Тигр"));
-        System.out.println(stringList.add("Лев"));
-        System.out.println(stringList.add("Утка"));
-        System.out.println(stringList.add("Жираф"));
-        System.out.println(stringList.add("Слон"));
-        System.out.println(stringList.add("Волк"));
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(6);
+        myList.add(7);
 
-        System.out.println(stringList.add(4, "Бегемот"));//2
-        System.out.println(stringList.set(6, "Крокодил"));
-        System.out.println(stringList.remove(1));
-        System.out.println(stringList.remove("Лев"));
-        System.out.println(stringList.contains("Медведь"));
-        System.out.println(stringList.indexOf("Волк"));
-        System.out.println(stringList.lastIndexOf("Жираф"));
-        System.out.println(stringList.get(4));
-        System.out.println(stringList.size());
-        System.out.println(stringList.isEmpty());
-        System.out.println(Arrays.toString(stringList.toArray()));
-        stringList.clear();
-        System.out.println(Arrays.toString(stringList.toArray()));
+
+
+        for (Integer value : myList) {
+            System.out.println(value);
+
+        }
+
+        myList.iterator().forEachRemaining(System.out::println);
     }
 }
